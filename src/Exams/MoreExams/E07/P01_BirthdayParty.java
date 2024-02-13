7fcc0624387333804d4736b8867c1a9c913d6367
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-13 15:32:39 +0200

    Create P01_BirthdayParty.java

---
+++ src/Exams/MoreExams/E07/P01_BirthdayParty.java
@@ -0,0 +1,19 @@
+package Exams.MoreExams.E07;
+
+import java.util.Scanner;
+
+public class P01_BirthdayParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double rent = Double.parseDouble(scanner.nextLine());
+
+        double cakePrice = rent * 0.2;
+        double drinksPrice = cakePrice * 0.55; // Напитките са 45% по-малко от цената на тортата
+        double animatorPrice = rent / 3;
+
+        double totalBudget = rent + cakePrice + drinksPrice + animatorPrice;
+
+        System.out.printf("%.2f", totalBudget);
+    }
+}
