243d6353cd82dad46634fd47c8575bae6d1be233
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-13 13:09:15 +0200

    Create P02_Safari.java

---
+++ src/Exams/MoreExams/E04/P02_Safari.java
@@ -0,0 +1,32 @@
+package Exams.MoreExams.E04;
+
+import java.util.Scanner;
+
+public class P02_Safari {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double budget = Double.parseDouble(scanner.nextLine());
+        double fuelLiters = Double.parseDouble(scanner.nextLine());
+        String dayOfWeek = scanner.nextLine();
+
+        double fuelPrice = 2.10;
+        double guidePrice = 100.00;
+
+        double totalExpenses = fuelPrice * fuelLiters + guidePrice;
+
+        if (dayOfWeek.equals("Saturday")) {
+            totalExpenses *= 0.90; // 10% discount
+        } else if (dayOfWeek.equals("Sunday")) {
+            totalExpenses *= 0.80; // 20% discount
+        }
+
+        if (budget >= totalExpenses) {
+            double moneyLeft = budget - totalExpenses;
+            System.out.printf("Safari time! Money left: %.2f lv.", moneyLeft);
+        } else {
+            double moneyNeeded = totalExpenses - budget;
+            System.out.printf("Not enough money! Money needed: %.2f lv.", moneyNeeded);
+        }
+    }
+}
